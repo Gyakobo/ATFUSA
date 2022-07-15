e1d5e85159ed8029f558cd73267f5985174d0969
Gyakobo <andygyakobo@gmail.com>
2022-07-15 02:16:19 -0400

    Found a solution with proxies, but there must be a way better one

--- src/components/sport_selection.jsx
+++ src/components/sport_selection.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+//import axios from 'axios';
 
 class SportSelection extends Component {
     state = {
-        url: "https://www.atfusa.org/webappTracking/MainMenu.json",
-        tags: [],
+        url: "https://www.atfusa.org/webappTracking/JNC/FldEventMenu.json",
     } 
 
+
     fetch_data() {
-        //const { url } = this.state;
-        fetch('https://www.atfusa.org/webappTracking/MainMenu.json')
-            .then(response => response.json())
-            .then(json => console.log(json))
+        let api = "https://www.atfusa.org/";
+        let url = "webappTracking/JNC/FldEventMenu.json";
+
+        const reqBody = {
+            "username": "Andrew",
+            "password": "123456"
+        }
+
+        fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "post",
+            body: JSON.stringify(reqBody),
+        }).then((response) => { 
+            return response.json() 
+        }).then((data) => console.log(data));
     };
+
 
     render() { 
         return (
